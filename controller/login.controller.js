648d17279d4373856d3280f7beaca63456f2c939
rachelYeffe <r0548431830@gmail.com>
2024-07-03 01:18:05 +0300

    add login controller

---
+++ controller/login.controller.js
@@ -0,0 +1,101 @@
+const { Router } = require('express');
+const userService = require('../service/login.service');
+const router = Router();
+
+/**
+ * @swagger
+ * tags:
+ *   name: Authentication
+ *   description: User authentication operations
+ */
+
+/**
+ * @swagger
+ * /signin:
+ *   post:
+ *     summary: Sign in a user
+ *     tags: [Authentication]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *         
+ *     responses:
+ *       '200':
+ *         description: Successful sign in
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       '500':
+ *         description: Internal server error
+ */
+router.post('/signin', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await userService.signIn({ email, password });
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.send(user);
+    } catch (error) {
+        console.error(`Error in signing in: ${error.message}`);
+        res.status(500).send(`Error in signing in: ${error.message}`);
+    }
+});
+
+
+
+
+
+
+
+
+
+
+/**
+ * @swagger
+ * /signup:
+ *   post:
+ *     summary: Sign up a new user
+ *     tags: [Authentication]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       '201':
+ *         description: Successful sign up
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       '500':
+ *         description: Internal server error
+ */
+router.post('/signup', async (req, res) => {
+    try {
+        console.log("lll");
+        const userData = req.body;
+        console.log(userData);
+        const newUser = await userService.signUp(userData);
+        console.log("ggggggg");
+        res.status(201).json(newUser);
+    } catch (error) {
+        console.error(`Error in signing up: ${error.message}`);
+        res.status(500).send(`Error in signing up: ${error.message}`);
+    }
+});
+
+module.exports = router;
+
+
